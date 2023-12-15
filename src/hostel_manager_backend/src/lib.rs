d7262073d503c6598dfc820a0e19124974887db7
Adophilus <uchenna19of@gmail.com>
2023-12-15 08:49:51 +0000

    separated out some components to different files

--- src/hostel_manager_backend/src/lib.rs
+++ src/hostel_manager_backend/src/lib.rs
@@ -1,184 +1,118 @@
-use candid::{Decode, Encode};
 use ic_stable_structures::memory_manager;
-use std::{borrow, cell};
+use ic_stable_structures::StableBTreeMap;
+use std::cell;
 
-type Memory =
-    ic_stable_structures::memory_manager::VirtualMemory<ic_stable_structures::DefaultMemoryImpl>;
-type IdCell = ic_stable_structures::Cell<u64, Memory>;
+mod error;
+mod models;
 
-#[derive(candid::CandidType, serde::Serialize, serde::Deserialize)]
-enum Error {
-    RoomNotFound,
-    RoomNotAvailable,
-    RoomFull,
-    RoomAlreadyBooked,
-    InsufficientPrice,
-    NotOwner,
-    NotInRoom
-}
-
-#[derive(candid::CandidType, serde::Serialize, serde::Deserialize, Clone, PartialEq, Eq)]
-struct User(String);
-
-#[derive(candid::CandidType, serde::Serialize, serde::Deserialize, Clone)]
-struct MyVec<T>(Vec<T>);
-
-#[derive(candid::CandidType, serde::Serialize, serde::Deserialize, Clone, PartialEq, Eq)]
-enum RoomState {
-    Full,
-    PartiallyVacant,
-    TotallyVacant,
-}
-
-#[derive(candid::CandidType, serde::Serialize, serde::Deserialize, Clone)]
-struct Room {
-    no: u64,
-    capacity: usize,
-    price_per_occupant: usize,
-    state: RoomState,
-    occupants: MyVec<User>,
-    owner: User,
-}
-
-impl ic_stable_structures::Storable for User {
-    fn to_bytes(&self) -> std::borrow::Cow<[u8]> {
-        borrow::Cow::Owned(Encode!(self).unwrap())
-    }
-
-    fn from_bytes(bytes: std::borrow::Cow<[u8]>) -> Self {
-        Decode!(bytes.as_ref(), Self).unwrap()
-    }
-}
-
-impl ic_stable_structures::BoundedStorable for User {
-    const MAX_SIZE: u32 = 1024;
-    const IS_FIXED_SIZE: bool = false;
-}
-
-impl ic_stable_structures::Storable for Room {
-    fn to_bytes(&self) -> std::borrow::Cow<[u8]> {
-        borrow::Cow::Owned(Encode!(self).unwrap())
-    }
-
-    fn from_bytes(bytes: std::borrow::Cow<[u8]>) -> Self {
-        Decode!(bytes.as_ref(), Self).unwrap()
-    }
-}
-
-impl ic_stable_structures::BoundedStorable for Room {
-    const MAX_SIZE: u32 = 1024;
-    const IS_FIXED_SIZE: bool = false;
-}
+type Memory = memory_manager::VirtualMemory<ic_stable_structures::DefaultMemoryImpl>;
+// type IdCell = ic_stable_structures::Cell<u64, Memory>;
 
 thread_local! {
     static MEMORY_MANAGER: cell::RefCell<memory_manager::MemoryManager<ic_stable_structures::DefaultMemoryImpl>> = cell::RefCell::new(memory_manager::MemoryManager::init(ic_stable_structures::DefaultMemoryImpl::default()));
-    static ROOM_COUNTER: cell::RefCell<IdCell> = cell::RefCell::new(IdCell::init(MEMORY_MANAGER.with(|m| m.borrow().get(memory_manager::MemoryId::new(0))), 1).expect("Failed to initialize ROOM counter"));
+    // static ROOM_COUNTER: cell::RefCell<IdCell> = cell::RefCell::new(IdCell::init(MEMORY_MANAGER.with(|m| m.borrow().get(memory_manager::MemoryId::new(0))), 1).expect("Failed to initialize ROOM counter"));
     // static ROOMS: cell::RefCell<StableVec<Room>> = cell::RefCell::new(StableVec::init(MEMORY_MANAGER.with(|m| m.borrow().get(memory_manager::MemoryId::new(1)))).expect("Failed to initialize vector!"));
-    static ROOMS: cell::RefCell<MyVec<Room>> = cell::RefCell::new(MyVec(vec![]));
+    static ROOMS: cell::RefCell<StableBTreeMap<u64, models::Room, Memory>> = cell::RefCell::new(StableBTreeMap::init(MEMORY_MANAGER.with(|m| m.borrow().get(memory_manager::MemoryId::new(1)))));
 }
 
 #[ic_cdk::query]
-fn get_rooms() -> Vec<Room> {
-    ROOMS.with(|r| r.borrow().0.clone())
+fn get_rooms() -> Vec<models::Room> {
+    ROOMS.with(|r| r.borrow().iter().map(|(_, room)| room.clone()).collect())
 }
 
 #[ic_cdk::query]
-fn get_room_by_number(room_no: u64) -> Result<Room, Error> {
+fn get_room_by_number(
+    payload: models::GetRoomByNumberPayload,
+) -> Result<models::Room, error::Error> {
+    ROOMS.with(|r| {
+        let room = r
+            .borrow()
+            .get(&payload.number)
+            .ok_or(error::Error::RoomNotFound)?;
+        Ok(room.clone())
+    })
+}
+
+#[ic_cdk::update]
+fn create_room(payload: models::CreateRoomPayload) -> Result<String, error::Error> {
+    let room = models::Room {
+        no: payload.number,
+        capacity: payload.capacity,
+        price_per_occupant: payload.price_per_occupant,
+        state: models::RoomState::TotallyVacant,
+        occupants: models::CustomVec(vec![]),
+        owner: models::User(ic_cdk::caller().to_string()),
+    };
+
+    ROOMS.with(|r| {
+        let mut rooms = r.borrow_mut();
+        if rooms.contains_key(&room.no) {
+            return Err(error::Error::RoomAlreadyExists);
+        }
+        rooms.insert(room.no, room);
+        Ok(String::from("Room created successfully!"))
+    })
+}
+
+#[ic_cdk::update]
+fn book_room(payload: models::BookRoomPayload) -> Result<String, error::Error> {
     ROOMS.with(|r| {
         let rooms = r.borrow();
-        match rooms.0.iter().find(|r| r.no == room_no) {
-            Some(room) => Ok(room.clone()),
-            None => Err(Error::RoomNotFound),
+        let mut room = rooms
+            .get(&payload.number)
+            .ok_or(error::Error::RoomNotFound)?;
+
+        if room.state == models::RoomState::Full {
+            return Err(error::Error::RoomFull);
+        }
+
+        if payload.price < room.price_per_occupant {
+            return Err(error::Error::InsufficientPrice);
+        }
+
+        let occupant = models::User(ic_cdk::caller().to_string());
+
+        match room.add_occupant(occupant) {
+            Ok(_) => Ok(String::from("Room successfully booked")),
+            Err(err) => Err(err),
         }
     })
 }
 
 #[ic_cdk::update]
-fn create_room(capacity: usize, price_per_occupant: usize) -> () {
-    ROOM_COUNTER.with(|c| {
-        let room_number = *c.borrow().get();
-        let room = Room {
-            no: room_number,
-            capacity,
-            price_per_occupant,
-            state: RoomState::TotallyVacant,
-            occupants: MyVec(vec![]),
-            owner: User(ic_cdk::caller().to_string()),
-        };
+fn unbook_room(payload: models::UnbookRoomPayload) -> Result<String, error::Error> {
+    ROOMS.with(|r| {
+        let rooms = r.borrow_mut();
+        let mut room = rooms
+            .get(&payload.number)
+            .ok_or(error::Error::RoomNotFound)?;
+        let occupant = models::User(ic_cdk::caller().to_string());
 
-        ROOMS.with(|r| r.borrow_mut().0.push(room));
+        match room.has_occupant(occupant.clone()) {
+            Some(_) => match room.remove_occupant(occupant) {
+                Ok(_) => Ok(String::from("Room unbooked successfully!")),
+                Err(_) => Err(error::Error::NotInRoom),
+            },
+            None => Err(error::Error::NotInRoom),
+        }
     })
 }
 
 #[ic_cdk::update]
-fn book_room(room_no: u64, price: usize) -> Result<(), Error> {
+fn delete_room(payload: models::DeleteRoomPayload) -> Result<String, error::Error> {
     ROOMS.with(|r| {
         let mut rooms = r.borrow_mut();
-        let room = match rooms.0.iter_mut().find(|r| r.no == room_no) {
-            Some(room) => room,
-            None => return Err(Error::RoomNotAvailable),
-        };
+        let room = rooms
+            .get(&payload.number)
+            .ok_or(error::Error::RoomNotFound)?;
 
-        if room.state == RoomState::Full {
-            return Err(Error::RoomFull);
+        if room.owner != models::User(ic_cdk::caller().to_string()) {
+            return Err(error::Error::NotOwner);
         }
 
-        if price < room.price_per_occupant {
-            return Err(Error::InsufficientPrice);
-        }
+        rooms.remove(&payload.number);
 
-        let occupant = User(ic_cdk::caller().to_string());
-
-        match room.occupants.0.iter().find(|o| **o == occupant) {
-            Some(_) => {
-                room.occupants.0.push(occupant);
-                Ok(())
-            }
-            None => return Err(Error::RoomAlreadyBooked),
-        }
-    })
-}
-
-#[ic_cdk::update]
-fn unbook_room(room_no: u64) -> Result<(), Error> {
-    ROOMS.with(|r| {
-        let mut rooms = r.borrow_mut();
-        let room = match rooms.0.iter_mut().find(|r| r.no == room_no) {
-            Some(room) => room,
-            None => return Err(Error::RoomNotAvailable),
-        };
-
-        let occupant = User(ic_cdk::caller().to_string());
-
-        match room.occupants.0.iter().position(|o| *o == occupant) {
-            Some(index) => {
-                room.occupants.0.remove(index);
-                Ok(())
-            }
-            None => Err(Error::NotInRoom),
-        }
-    })
-}
-
-#[ic_cdk::update]
-fn delete_room(room_no: u64) -> Result<(), Error> {
-    ROOMS.with(|r| {
-        let mut rooms = r.borrow_mut();
-        let room_index = rooms
-            .0
-            .iter()
-            .position(|room| room.no == room_no)
-            .ok_or(Error::RoomNotAvailable)?;
-
-        let room = &rooms.0[room_index];
-
-        if room.owner != User(ic_cdk::caller().to_string()) {
-            return Err(Error::NotOwner);
-        }
-
-        rooms.0.remove(room_index);
-
-        Ok(())
+        Ok(String::from("Room deleted successfully!"))
     })
 }
 
---
+++ src/hostel_manager_backend/src/models.rs
@@ -0,0 +1,111 @@
+use candid::{Decode, Encode};
+use std::borrow;
+
+use crate::error;
+
+#[derive(candid::CandidType, serde::Serialize, serde::Deserialize, Clone, PartialEq, Eq)]
+pub struct User(pub String);
+
+#[derive(candid::CandidType, serde::Serialize, serde::Deserialize, Clone)]
+pub struct CustomVec<T>(pub Vec<T>);
+
+#[derive(candid::CandidType, serde::Serialize, serde::Deserialize, Clone, PartialEq, Eq)]
+pub enum RoomState {
+    Full,
+    PartiallyVacant,
+    TotallyVacant,
+}
+
+#[derive(candid::CandidType, serde::Serialize, serde::Deserialize, Clone)]
+pub struct Room {
+    pub no: u64,
+    pub capacity: u64,
+    pub price_per_occupant: u64,
+    pub state: RoomState,
+    pub occupants: CustomVec<User>,
+    pub owner: User,
+}
+
+impl Room {
+    pub fn has_occupant(&self, occupant: User) -> Option<usize> {
+        self.occupants.0.iter().position(|o| o == &occupant)
+    }
+
+    pub fn add_occupant(&mut self, occupant: User) -> Result<(), error::Error> {
+        match self.has_occupant(occupant.clone()) {
+            Some(_) => Err(error::Error::RoomAlreadyBooked),
+            None => {
+                self.occupants.0.push(occupant);
+                Ok(())
+            }
+        }
+    }
+
+    pub fn remove_occupant(&mut self, occupant: User) -> Result<(), error::Error> {
+        match self.has_occupant(occupant) {
+            Some(index) => {
+                self.occupants.0.remove(index);
+                Ok(())
+            }
+            None => Err(error::Error::NotInRoom),
+        }
+    }
+}
+
+impl ic_stable_structures::Storable for User {
+    fn to_bytes(&self) -> std::borrow::Cow<[u8]> {
+        borrow::Cow::Owned(Encode!(self).unwrap())
+    }
+
+    fn from_bytes(bytes: std::borrow::Cow<[u8]>) -> Self {
+        Decode!(bytes.as_ref(), Self).unwrap()
+    }
+}
+
+impl ic_stable_structures::BoundedStorable for User {
+    const MAX_SIZE: u32 = 1024;
+    const IS_FIXED_SIZE: bool = false;
+}
+
+impl ic_stable_structures::Storable for Room {
+    fn to_bytes(&self) -> std::borrow::Cow<[u8]> {
+        borrow::Cow::Owned(Encode!(self).unwrap())
+    }
+
+    fn from_bytes(bytes: std::borrow::Cow<[u8]>) -> Self {
+        Decode!(bytes.as_ref(), Self).unwrap()
+    }
+}
+
+impl ic_stable_structures::BoundedStorable for Room {
+    const MAX_SIZE: u32 = 1024;
+    const IS_FIXED_SIZE: bool = false;
+}
+
+#[derive(candid::CandidType, serde::Serialize, serde::Deserialize)]
+pub struct GetRoomByNumberPayload {
+    pub number: u64,
+}
+
+#[derive(candid::CandidType, serde::Serialize, serde::Deserialize)]
+pub struct CreateRoomPayload {
+    pub number: u64,
+    pub capacity: u64,
+    pub price_per_occupant: u64,
+}
+
+#[derive(candid::CandidType, serde::Serialize, serde::Deserialize)]
+pub struct BookRoomPayload {
+    pub number: u64,
+    pub price: u64,
+}
+
+#[derive(candid::CandidType, serde::Serialize, serde::Deserialize)]
+pub struct UnbookRoomPayload {
+    pub number: u64,
+}
+
+#[derive(candid::CandidType, serde::Serialize, serde::Deserialize)]
+pub struct DeleteRoomPayload {
+    pub number: u64,
+}
